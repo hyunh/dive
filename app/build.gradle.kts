e4799a799855bb27f889d505ef7107553685b78e
Hyunhae Lee <hyunhae.lee@gmail.com>
2022-09-26 02:07:14 +0900

    Update compileSdk and targetSdk 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "hyunh.dive"
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "hyunh.dive"
         minSdk = 24
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "0.1"
 
